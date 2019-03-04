0c7b8950ae7d8c6c1ca5a22c14799f88e355c452
jpyatachkov <cvkucherov@yandex.ru>
2019-03-04 10:04:29 +0300

    Added default value to course is_active flag

--- db/migrate/20190302122231_add_active_falg_to_course.rb
+++ db/migrate/20190302122231_add_active_falg_to_course.rb
@@ -1,5 +1,5 @@
 class AddActiveFalgToCourse < ActiveRecord::Migration[5.2]
   def change
-    add_column :courses, :is_active, :bool
+    add_column :courses, :is_active, :bool, default: true
   end
 end
