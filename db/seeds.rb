b4146f7717bc1af2de9bfc577f442e4a507effab
jpyatachkov <cvkucherov@yandex.ru>
2019-03-13 00:31:02 +0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# WARNING!
+# Provide SECRET_PASSWORD env variable value before run rake db:seed!
+
+alexander = User.first_or_create first_name: 'Александр',
+                                 last_name: 'Барулев',
+                                 username: 'a.barulev',
+                                 email: 'a.barulev@mail.ru',
+                                 password: ENV['SECRET_PASSWORD'],
+                                 password_confirmation: ENV['SECRET_PASSWORD']
+maxim = User.first_or_create first_name: 'Максим',
+                             last_name: 'Колотовкин',
+                             username: 'm.kolotovkin',
+                             email: 'm.kolotovkin@mail.ru',
+                             password: ENV['SECRET_PASSWORD'],
+                             password_confirmation: ENV['SECRET_PASSWORD']
+User.create first_name: 'Кирилл',
+            last_name: 'Кучеров',
+            username: 'k.kucherov',
+            email: 'k.kucherov@mail.ru',
+            password: ENV['SECRET_PASSWORD'],
+            password_confirmation: ENV['SECRET_PASSWORD']
+
+creator_ids = [alexander.id, maxim.id]
+
+100.times do
+  current_user_id = creator_ids.sample(1).first
+
+  course = Course.create title: Faker::Lorem.sentence,
+                         description: Faker::Lorem.paragraph,
+                         user_id: current_user_id
+
+  Faker::Number.between(7, 19).times do
+    Assignment.create text: Faker::Lorem.paragraph,
+                      course_id: course.id,
+                      user_id: current_user_id
+  end
+end
