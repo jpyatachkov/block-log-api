14a6300c4fd00671092709925b795049dfa51ac3
HustonMmmavr <huston.mavr@gmail.com>
2019-03-14 03:54:25 +0300

    Now output for exercise doesnt return

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,4 +1,10 @@
 class AssignmentSerializer < ActiveModel::Serializer
   attributes :id, :text, :tests
   has_one :course
+
+  def tests
+    # wtf ???
+    # it doesnt accpet :inputs
+    object.tests.slice('inputs')
+  end
 end
