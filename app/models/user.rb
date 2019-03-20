af39fcca124edd2622dd1517e139b23f897e7cd0
jpyatachkov <cvkucherov@yandex.ru>
2019-03-20 16:55:35 +0300

    Added is_staff field and admin translations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  after_create :assign_default_role
+  after_create :assign_default_role,
+               :process_is_staff_flag
+  after_update :process_is_staff_flag
 
   def self.from_token_request(request)
     find_by_username request.params.dig 'auth', 'username'
@@ -23,5 +25,13 @@ class User < ApplicationRecord
 
   def assign_default_role
     add_role(:user, Course) if roles.blank?
+  end
+
+  def process_is_staff_flag
+    if is_staff
+      add_role :moderator, Course
+    else
+      remove_role :moderator, Course
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_092416) do
+ActiveRecord::Schema.define(version: 2019_03_20_130127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 2019_03_16_092416) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.boolean "is_staff", default: false
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
