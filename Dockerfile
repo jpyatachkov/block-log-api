6a7f91dd04cf75df0acf1fd07317c26639d11a84
jpyatachkov <cvkucherov@yandex.ru>
2019-03-21 21:15:44 +0300

    Dockerization

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ruby:2.6
+
+RUN apt-get update && \
+    apt-get install -qq -y build-essential libpq-dev postgresql-client --fix-missing --no-install-recommends
+
+ENV RACK_ENV production
+ENV RAILS_ENV production
+ENV RAILS_ROOT /var/www/block-log-api
+
+RUN mkdir -p $RAILS_ROOT shared/pids shared/sockets shared/log
+WORKDIR $RAILS_ROOT
+
+COPY Gemfile Gemfile.lock ./
+RUN gem install bundler && bundle install --binstubs --jobs 20 --retry 5 --without development test
+
+COPY . .
+
+EXPOSE 3000
+
+CMD bundle exec puma -C config/puma.rb
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -10,8 +9,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +18,11 @@ module BlockLogApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.log_tags = [:subdomain, :uuid]
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
