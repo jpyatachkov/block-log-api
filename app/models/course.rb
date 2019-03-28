f52a59448a355a0b5403e38777e1101d78a31209
HustonMmmavr <hustom.mavr@gmail.com>
2019-03-28 23:43:51 +0300

    Removed uniqueness from course; Added partial index to db; Added begin resque check to course create and update

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,10 +3,7 @@ class Course < ApplicationRecord
 
   has_many :commentary
 
-  validates :title, presence: true #, uniqueness: true
-  validates :title, uniqueness: true, if: :validate_before_create, on: :create
-  validates :title, uniqueness: true, if: :validate_before_update, on: :updaete  #:active_course_exists#, on: :create
-  
+  validates :title, presence: true
   validates :description, presence: true
 
   after_create :set_user_permissions,
@@ -28,26 +25,4 @@ class Course < ApplicationRecord
     user = User.find(user_id)
     CourseUser.create!(user: user, course: self)
   end
-
-  def validate_before_create
-    course = Course.where(title: title, is_active: true).count
-    return course == 0 ? false : true
-  end
-
-  # refactor
-  def validate_before_update
-    if title.present? # on delete there is no title
-      p title
-      course = Course.where(title: title, is_active: true).first
-      p course.id
-      if course.nil?
-        p 'nil'
-        return false
-      elsif course.id == id
-        p 'id eq'
-        return false
-      end
-      return true
-    end
-  end 
 end
