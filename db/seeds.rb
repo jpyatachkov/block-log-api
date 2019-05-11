a25148960408551a0f5c198476b7eccddf27d8aa
jpyatachkov <cvkucherov@yandex.ru>
2019-05-12 00:02:17 +0300

    Seed only users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,23 @@
 # WARNING!
 # Provide SECRET_PASSWORD env variable value before run rake db:seed!
 
-alexander = User.first_or_create first_name: 'Александр',
-                                 last_name: 'Барулев',
-                                 username: 'a.barulev',
-                                 email: 'a.barulev@mail.ru',
-                                 password: ENV['SECRET_PASSWORD'],
-                                 password_confirmation: ENV['SECRET_PASSWORD']
-maxim = User.first_or_create first_name: 'Максим',
-                             last_name: 'Колотовкин',
-                             username: 'm.kolotovkin',
-                             email: 'm.kolotovkin@mail.ru',
-                             password: ENV['SECRET_PASSWORD'],
-                             password_confirmation: ENV['SECRET_PASSWORD']
+User.first_or_create first_name: 'Александр',
+                     last_name: 'Барулев',
+                     username: 'a.barulev',
+                     email: 'a.barulev@mail.ru',
+                     password: ENV['SECRET_PASSWORD'],
+                     password_confirmation: ENV['SECRET_PASSWORD'],
+                     is_staff: true,
+                     is_admin: true
+User.first_or_create first_name: 'Максим',
+                     last_name: 'Колотовкин',
+                     username: 'm.kolotovkin',
+                     email: 'm.kolotovkin@mail.ru',
+                     password: ENV['SECRET_PASSWORD'],
+                     password_confirmation: ENV['SECRET_PASSWORD']
 User.create first_name: 'Кирилл',
             last_name: 'Кучеров',
             username: 'k.kucherov',
             email: 'k.kucherov@mail.ru',
             password: ENV['SECRET_PASSWORD'],
             password_confirmation: ENV['SECRET_PASSWORD']
-
-creator_ids = [alexander.id, maxim.id]
-
-100.times do
-  current_user_id = creator_ids.sample(1).first
-
-  course = Course.create title: Faker::Lorem.sentence,
-                         short_description: Faker::Lorem.paragraph,
-                         description: Faker::Lorem.paragraph,
-                         user_id: current_user_id
-
-  Faker::Number.between(3, 29).times do
-    Assignment.create title: Faker::Lorem.word,
-                      description: Faker::Lorem.paragraph,
-                      course_id: course.id,
-                      user_id: current_user_id
-  end
-end
